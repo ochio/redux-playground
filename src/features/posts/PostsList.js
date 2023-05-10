fb0146bf650737b4bdb2cc50b25d262004bf844b
ochi <ocos54ce@gmail.com>
2023-05-11 00:30:31 +0900

    tutorial6

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { selectAllPosts, fetchPosts } from './postsSlice'
+import {
+  selectAllPosts,
+  fetchPosts,
+  selectPostIds,
+  selectPostById
+} from './postsSlice'
 import { Link } from 'react-router-dom'
 import {ReactionButtons} from "./ReactionButtons"
 import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 
-const PostExcerpt = ({ post }) => {
+let PostExcerpt = ({ postId }) => {
+  const post = useSelector(state => selectPostById(state, postId))
   return (
     <article className="post-excerpt">
       <h3>{post.title}</h3>
@@ -27,7 +33,7 @@ const PostExcerpt = ({ post }) => {
 
 export const PostsList = () => {
 	const dispatch = useDispatch()
-	const posts = useSelector(selectAllPosts)
+	const orderedPostIds = useSelector(selectPostIds)
 	const postStatus = useSelector(state => state.posts.status)
 	const error = useSelector(state => state.posts.error)
 
@@ -43,13 +49,9 @@ export const PostsList = () => {
     content = <Spinner text="Loading..." />
   } else if (postStatus === 'succeeded') {
     // Sort posts in reverse chronological order by datetime string
-    const orderedPosts = posts
-      .slice()
-      .sort((a, b) => b.date.localeCompare(a.date))
-
-    content = orderedPosts.map(post => (
-      <PostExcerpt key={post.id} post={post} />
-    ))
+		content = orderedPostIds.map(postId => (
+			<PostExcerpt key={postId} postId={postId} />
+		))
   } else if (postStatus === 'failed') {
     content = <div>{error}</div>
   }
